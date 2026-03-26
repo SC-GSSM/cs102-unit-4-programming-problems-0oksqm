c3ba64839c889e100b672f3d2528d933ab5e7b0f
Samantha M <0oksqm@gmail.com>
2026-03-26 23:10:34 +0000

    modified:   src/main/java/unit4/Unit4.java

--- src/main/java/unit4/Unit4.java
+++ src/main/java/unit4/Unit4.java
@@ -183,7 +183,7 @@ public class Unit4 {
         }
         System.out.print("\n");
         
-        for(int i = 0; i <= n; i++){
+        for(int i = 0; i < n; i++){
             for(int j = 0; j < n; j++){
                 System.out.print("*");
             }
